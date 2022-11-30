a043c57f1fae77f13a4cd0ad8ec931eae24a7c2f
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-11-30 11:01:08 -0700

    updated images

--- js/index.js
+++ js/index.js
@@ -89,7 +89,37 @@
       window.addEventListener('load', headerScrolled)
       onscroll(document, headerScrolled)
     }
-
+    $(document).ready(function(){
+      $(".owl-carousel").owlCarousel({
+          loop:true,
+          autoplay:true,
+          nav:false,
+          responsiveClass:true,
+          dotsEach:true,
+          margin:10,
+          responsive:{
+              0:{
+                  items:2,
+                  
+              },
+              600:{
+                  items:4,
+                  nav:false
+              },
+              1000:{
+                  items:6,
+              }
+          }
+      
+      });
+  
+  });
+  
+  /*$(".owl-carousel").owlCarousel({
+      loop:true,
+      autoplay:true,
+      nav:true,
+  });*/
 
 /*$(".owl-carousel").owlCarousel({
     loop:true,
