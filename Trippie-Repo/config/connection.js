e051c700e8fc9918d7093f850dfdfe21ec161516
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2022-12-19 14:23:31 -0700

    updated files

---
+++ Trippie-Repo/config/connection.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  try {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "127.0.0.1",
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+module.exports = sequelize;
